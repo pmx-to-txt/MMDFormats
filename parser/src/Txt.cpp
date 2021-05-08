7c9dd2686cab1632568be530af199a44a582c147
Anonymous <example@foo.com>
2021-05-08 23:24:00 +1100

    エンコード出したけど日本語が文字化け

--- parser/src/Txt.cpp
+++ parser/src/Txt.cpp
@@ -6,10 +6,28 @@ using namespace std;
 
 void ExportVersion(std::ostream& stream, const pmx::Model& model)
 {
-	stream << "Version: " << fixed << setprecision(1) << model.version << defaultfloat << endl;
+	stream << "PMX バージョン: " << fixed << setprecision(1) << model.version << defaultfloat << endl;
+}
+
+using UEncoding = uint8_t;
+struct UnknownEncoding : exception {};
+string ToString(UEncoding encoding)
+{
+	switch (encoding)
+	{
+	case 0: return "UTF-16(LE)";
+	case 1: return "UTF-8";
+	default: throw UnknownEncoding{};
+	}
+}
+
+void ExportSetting(std::ostream& stream, const pmx::Model& model)
+{
+	stream << "文字エンコード: " << ToString(model.setting.encoding) << endl;
 }
 
 void pmx2txt::txt::Export(std::ostream& stream, const pmx::Model& model)
 {
 	ExportVersion(stream, model);
+	ExportSetting(stream, model);
 }
