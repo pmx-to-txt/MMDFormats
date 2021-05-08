13db909635df57cd2071049da8fa2eebabdfb57e
Anonymous <example@foo.com>
2021-05-08 23:50:45 +1100

    ExportModelInfo

--- parser/src/Txt.cpp
+++ parser/src/Txt.cpp
@@ -27,8 +27,23 @@ void ExportSetting(std::ostream& stream, const pmx::Model& model)
 	stream << "追加UV数: " << static_cast<int>(model.setting.uv) << endl;
 }
 
+void ExportModelInfo(std::ostream& stream, const pmx::Model& model)
+{
+	stream << "モデル名: " << model.model_name << endl;
+	stream << "コメント: " << endl;
+	stream << "----------------" << endl;
+	stream << model.model_comment << endl;
+	stream << "----------------" << endl;
+	stream << "モデル名(英): " << model.model_english_name << endl;
+	stream << "コメント(英): " << endl;
+	stream << "----------------" << endl;
+	stream << model.model_english_comment << endl;
+	stream << "----------------" << endl;
+}
+
 void pmx2txt::txt::Export(std::ostream& stream, const pmx::Model& model)
 {
 	ExportVersion(stream, model);
 	ExportSetting(stream, model);
+	ExportModelInfo(stream, model);
 }
