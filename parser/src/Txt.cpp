a0fe4ee48491192ff4caf3faefc466d2d7dc442d
Anonymous <example@foo.com>
2021-05-08 23:08:45 +1100

    バージョンを表示

--- parser/src/Txt.cpp
+++ parser/src/Txt.cpp
@@ -1,8 +1,10 @@
 #include "pmx2txt/parser/Txt.h"
 
+#include <iomanip>
+
 using namespace std;
 
 void pmx2txt::txt::Export(std::ostream& stream, const pmx::Model& model)
 {
-	stream << model.version << endl;
+	stream << "Version: " << fixed << setprecision(1) << model.version << defaultfloat << endl;
 }
